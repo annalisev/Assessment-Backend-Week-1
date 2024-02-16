6598176c2e169ee63aa5a7e45329c19c9542c17c
annalisev <annaliseverzijl@gmail.com>
2024-02-16 11:33:49 +0000

    Write convert_to_datetime

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -4,7 +4,11 @@ from datetime import datetime
 
 
 def convert_to_datetime(date: str) -> datetime:
-    pass
+    '''returns date in a datetime object'''
+    if not isinstance(date, str):
+        raise TypeError('Input is not string')
+    return (datetime.strptime(
+                date, '%d.%m.%Y'))
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
