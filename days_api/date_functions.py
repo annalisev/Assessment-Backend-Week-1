a7f8529656d43a1f8605331a9488db71181de5d2
annalisev <annaliseverzijl@gmail.com>
2024-02-16 11:54:53 +0000

    Improve pylint

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -10,9 +10,9 @@ def convert_to_datetime(date: str) -> datetime:
     try:
         return (datetime.strptime(
                 date, '%d.%m.%Y'))
-    except(ValueError):
+    except ValueError:
         raise ValueError("Unable to convert value to datetime.")
-    
+
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
