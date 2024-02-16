dac4e21291401f1afead44e2d97aacdaea9b99e9
annalisev <annaliseverzijl@gmail.com>
2024-02-16 14:04:24 +0000

    Deal with bigs in app_history

--- days_api/app.py
+++ days_api/app.py
@@ -8,10 +8,11 @@ from flask import Flask, Response, request, jsonify
 
 from date_functions import convert_to_datetime, get_day_of_week_on, get_days_between
 
-app_history = []
+
 
 app = Flask(__name__)
-
+app_history = []
+print(app_history )
 
 def add_to_history(current_request):
     """Adds a route to the app history."""
@@ -54,25 +55,31 @@ def get_weekday():
     return jsonify({ "weekday": get_day_of_week_on(first)}), 200
 
 
-@app.route("/history", methods =["GET", "POST"])
+@app.route("/history", methods =["GET", "DELETE"])
 def get_or_delete_history():
     """Returns details on the last number of requests to the API"""
+    global app_history
+    add_to_history(request)
     if request.method == "GET":
-        num = int(request.args["number"]) if "number" in request.args else 5
+        limit = 5
+        if "number" in request.args:
+            limit = request.args["number"]
+            if not limit.isdigit():
+                return jsonify({'error': True,
+                            'message': 'number is not an integer'}), 400
+            limit = int(limit)
+            if limit < 1 or limit > 20:
+                return jsonify({'error': True,
+                            'message': 'number must be between 1 and 20'}), 400
+        num = min([limit, len(app_history)])
         return_arr =[]
         for i in range(num):
             return_arr.append(app_history[i])
         return jsonify(return_arr), 200
+    if request.method == "DELETE":
+        app_history = []
+        return jsonify({ "status": "History cleared" }), 200
 
-
-
-
-    try:
-        first = convert_to_datetime(request.json["date"])
-    except:
-        return jsonify({'error': True,
-                        'message': 'Could not convert to datetime, check format.'}), 400
-    return jsonify({ "weekday": get_day_of_week_on(first)}), 200
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
